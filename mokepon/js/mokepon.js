0b5527955f531a79c927d4b322ff7a9ac910c49c
kemirandad <kjmd2s@gmail.com>
2022-12-26 10:03:18 -0500

    Escuchando eventos con JavaScript

--- mokepon/js/mokepon.js
+++ mokepon/js/mokepon.js
@@ -1,6 +1,24 @@
-function seleccionarMascotaJugador(){
-    alert("Seleccionaste tu mascota")
+function iniciarJuego() {
+    let botonMascotaJugador = document.getElementById('boton-mascota')
+    botonMascotaJugador.addEventListener('click', seleccionarMascotaJugador)
 }
 
-let botonMascotaJugador = document.getElementById('boton-mascota')
-botonMascotaJugador.addEventListener('click', seleccionarMascotaJugador)
+function estaSeleccionadaMascota(nombreMascota) {
+    return document.getElementById(nombreMascota).checked
+}
+
+function seleccionarMascotaJugador(){
+    if (estaSeleccionadaMascota('hipodoge')) {
+        return alert("Seleccionaste a Hipodoge")
+    } else if (estaSeleccionadaMascota('capipepo')){
+        return alert("Seleccionaste a Capipepo")
+    } else if (estaSeleccionadaMascota('ratigueya')){
+        return alert("Seleccionaste a Ratigueya")
+    } else {
+        return alert("No has seleccionado a ninguna mascota")
+    }
+}
+
+
+
+window.addEventListener('load', iniciarJuego)
